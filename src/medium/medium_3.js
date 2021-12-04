98b37a28fe28a5d9cd6592174ffd6fb22a3f0fee
Jordan Aasman <jjord@ad.unc.edu>
2021-12-04 13:15:04 -0500

    more medium

--- src/medium/medium_3.js
+++ src/medium/medium_3.js
@@ -34,7 +34,8 @@ export function searchHighPower(car_data, minHorsepower, minTorque) {
  *
  */
 export function searchMpg(car_data, minCity, minHighway) {
-
+    let array = car_data.filter((object) => object.city_mpg >= minCity && object.highway_mpg >= minHighway)
+    return array.sort((x,y) => (x.highway_mpg < y.highway_mpg) ? 1 : -1).reverse();
 }
 
 
