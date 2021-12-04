0d602069b8750129db1ae38116c943f4cf2d0d3f
Jordan Aasman <jjord@ad.unc.edu>
2021-12-04 13:05:00 -0500

    more spicyyyyj

--- src/spicy/spicy_9.js
+++ src/spicy/spicy_9.js
@@ -153,6 +153,9 @@ export const filter = (arr, test) => {
     };
     for (let i=0; i<arr.length;i++) {
         if (test(arr[i])) {
+            answer.pass.push(arr[i]);
+        }
+        else {
             answer.fail.push(arr[i]);
         }
     }
@@ -166,8 +169,17 @@ export const filter = (arr, test) => {
  *   odd numbers. Use the "everyEven" function in this function.
  */
 export const allEvensAreOdd = (arr) => {
-    return everyEven(arr, isOdd);
+    return everyEven(arr, oddOrNot);
 };
+
+function oddOrNot(x) {
+    if (x % 2 == 1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
 
 
 /**
@@ -176,11 +188,7 @@ export const allEvensAreOdd = (arr) => {
  *   array is an odd number. Use the "someEven" function in this function.
  */
 export const anEvenIsOdd = (arr) => {
-    var x = filter(arr, test);
-    if (n == x.pass.length) {
-        return true;
-    }
-    return false;
+    return someEven(arr, oddOrNot);
 };
 
 
